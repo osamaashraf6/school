3129067c1e3ace1d9e46f96461f50e821f22f604
osamaashraf6 <osamaashraf668@gmail.com>
2023-02-17 00:21:13 +0200

    Modify the student class

--- student.rb
+++ student.rb
@@ -1,11 +1,19 @@
+require_relative 'person'
+
 class Student < Person
-    def initialize(age, classroom, parent_permission=true, name="Unknown")
-      super(age, parent_permission, name)
-      @classroom = classroom
-    end
-    
-    def play_hooky
-      "¯\\(ツ)/¯"
-    end
+  attr_reader :classroom 
+
+  def initialize(age, classroom, name: 'Unknown', parent_permission: true)
+    super(age, name: name, parent_permission: parent_permission)
+    @classroom = classroom
   end
-  
+
+  def play_hooky
+    '¯\\_(ツ)_/¯'.encode('utf-8')
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students << self
+  end
+end
