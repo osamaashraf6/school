b45de1d5e4381f13b80dcd4478fcb23e6d93b9cf
osamaashraf6 <osamaashraf668@gmail.com>
2023-02-16 22:56:18 +0200

    Add the teacher class

---
+++ teacher.rb
@@ -0,0 +1,11 @@
+class Teacher < Person
+    def initialize(age, specialization, parent_permission=true, name="Unknown")
+      super(age, parent_permission, name)
+      @specialization = specialization
+    end
+    
+    def can_use_services?
+      true
+    end
+  end
+  
