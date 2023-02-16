684533c6a7d775827200f150e51b2f517daa4f21
osamaashraf6 <osamaashraf668@gmail.com>
2023-02-16 23:31:33 +0200

    Add the trimmmer class for trimming and decorating the name

---
+++ trimmerDecorator.rb
@@ -0,0 +1,10 @@
+require_relative 'baseDecorator'
+
+class TrimmerDecorator < BaseDecorator
+    MAX_LENGTH = 10
+  
+    def correct_name
+      name = @nameable.correct_name
+      name.length > MAX_LENGTH ? name[0..MAX_LENGTH-1] : name
+    end
+  end
