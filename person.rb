30c5db765ab7e830849bc14d7a066d60433e012f
osamaashraf6 <osamaashraf668@gmail.com>
2023-02-16 23:33:24 +0200

    Add the person class

--- person.rb
+++ person.rb
@@ -1,20 +1,27 @@
-class Person
-    attr_reader :id
-    attr_accessor :name, :age
-    
-    def initialize(age, parent_permission=true, name="Unknown")
-      @id = rand(1..1000) # Generate a random ID between 1 and 1000
-      @name = name
-      @age = age
-      @parent_permission = parent_permission
-    end
-    
-    def is_of_age?
-      @age >= 18
-    end
-    
-    def can_use_services?
-      is_of_age? || @parent_permission
-    end
+require_relative 'nameable'
+
+class Person < Nameable
+  attr_reader :id, :age
+  attr_accessor :name
+
+  def initialize(age, parent_permission=true, name="Unknown")
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
   end
-  
+
+  def is_of_age?
+    @age >= 18
+  end
+
+  def can_use_services?
+    @parent_permission || is_of_age?
+  end
+
+  def correct_name
+    @name
+  end
+end
+
+
