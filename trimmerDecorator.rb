c871d568274f14ee15beff7191b9ca19f97c0874
osamaashraf6 <osamaashraf668@gmail.com>
2023-02-17 16:13:55 +0200

    Add the test for the  trimmer decorator class  and modify it

--- trimmerDecorator.rb
+++ trimmerDecorator.rb
@@ -1,10 +1,15 @@
 require_relative 'baseDecorator'
-
+# =========================
+# =========================
+# =========================
+# =========================
 class TrimmerDecorator < BaseDecorator
     MAX_LENGTH = 10
   
     def correct_name
       name = @nameable.correct_name
       name.length > MAX_LENGTH ? name[0..MAX_LENGTH-1] : name
+      # name.length > MAX_LENGTH ? name[0...MAX_LENGTH] : name
+
     end
   end
